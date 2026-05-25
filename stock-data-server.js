b3212fb25d292732d49f0bd246338c680cdd5e45
MaverickTopG <ayanshashish@gmail.com>
2026-05-25 14:02:52 -0700

    Extract stock request interval constant

--- stock-data-server.js
+++ stock-data-server.js
@@ -17,7 +17,8 @@ server.tool(
   async ({ symbol }) => {
     try {
       const apiKey = "YOUR_API_KEY";
-      const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=1min&apikey=${apiKey}`;
+      const interval = "1min";
+      const url = `https://www.alphavantage.co/query?function=TIME_SERIES_INTRADAY&symbol=${symbol}&interval=${interval}&apikey=${apiKey}`;
 
       const response = await axios.get(url);
       const timeSeries = response.data["Time Series (1min)"] || {};
